c8a58bb079d93eb1babf866e9453931839ee7fca
Koshelev <do3fh1@yandex.ru>
2020-08-11 19:57:53 +0300

    added comments

--- untitled/mainwindow.cpp
+++ untitled/mainwindow.cpp
@@ -31,57 +31,57 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    CSVList.clear();
+    CSVList.clear(); //Очистка массива для повторного чтения файла
     int i=0;
     ui->comboBox->clear();
-    QString fileName = QFileDialog::getOpenFileName(this, tr("Open .csv file"), "", tr(".csv (*.csv)"));
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open .csv file"), "", tr(".csv (*.csv)")); //Функция выбора файла .csv
     QFile file(fileName);
-    file.open(QIODevice::ReadOnly);
-    while (!file.atEnd()){
-        QString inputLine = file.readLine();
-        auto inputParts = inputLine.split(";", Qt::SkipEmptyParts);
-        if (inputParts[0]!="Tag"){
-            CSVList.append(new myList);
-            CSVList[i]->key=inputParts[0];
+    file.open(QIODevice::ReadOnly); //Открытие файла только для чтения
+    while (!file.atEnd()){ //Цикл на чтение файла, пока он не закончится
+        QString inputLine = file.readLine(); //Чтение построчно
+        auto inputParts = inputLine.split(";", Qt::SkipEmptyParts); //Разделение строки на слова, разделитель - ;
+        if (inputParts[0]!="Tag"){ //Чтобы в список не попадала строка с заголовками
+            CSVList.append(new myList); //Новая структура в массив
+            CSVList[i]->key=inputParts[0]; //Данные в массив
             CSVList[i]->value=inputParts[1];
-            ui->comboBox->addItem(inputParts[0]);
+            ui->comboBox->addItem(inputParts[0]); //Данные в список, без типа, только теги.
             i++;
         }
     }
-    file.close();
+    file.close(); //Закрытие файла
 }
 
 void MainWindow::on_pushButton_2_clicked()
 {
     jsonList.clear();
     ui->textBrowser->clear();
-    int matches=0;
+    int matches=0; //переменная для нахождения начала списка аргументов
     int offset=0;
     int k=0;
     QString findValue;
-    QString findKey = ui->comboBox->currentText();
-    for (int i=0;i<CSVList.size();i++){
+    QString findKey = ui->comboBox->currentText(); //Выбранный в списке тег
+    for (int i=0;i<CSVList.size();i++){ //Поиск в массиве тега и соответствующего ему типа
         if (CSVList[i]->key==findKey)
             findValue = CSVList[i]->value;
     }
-    QString fileName = QFileDialog::getOpenFileName(this, tr("Open .json file"), "", tr(".json (*.json)"));
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open .json file"), "", tr(".json (*.json)")); //Выбор файла .json
     QFile file(fileName);
-    file.open(QIODevice::ReadOnly);
+    file.open(QIODevice::ReadOnly); //Открытие в режиме чтения
     while(!file.atEnd()){
-        QString findString = file.readLine();
+        QString findString = file.readLine(); //Построчное чтение .json файла и происк нужного типа
         if (findString.contains(findValue)){
-            matches++;
+            matches++; //Если найден, переменная будет равна 1
         }
-        else if (findString.contains("}") and matches==1){
+        else if (findString.contains("}") and matches==1){ //Если нужный тип был найден и находится скобка закрытия списка агрументов - выход из цикла
             break;
         }
-        else if (((findString.contains("double")) or (findString.contains("int"))) and matches==1){
-            auto parts = findString.split(QRegExp("\\W+"), Qt::SkipEmptyParts);
+        else if (((findString.contains("double")) or (findString.contains("int"))) and matches==1){ //Если был найден нужный тип, происходит чтение строк с оффсетом double или int
+            auto parts = findString.split(QRegExp("\\W+"), Qt::SkipEmptyParts); //Разбиение строки на слова с отбросом всех символов
             jsonList.append((new myList));
             jsonList[k]->key=findKey+"."+parts[0];
             jsonList[k]->value=QVariant(offset).toString();
             k++;
-            if (parts[1]=="double")
+            if (parts[1]=="double") //Преобразование оффсета в число
                 offset+=8;
             else
                 offset+=4;
@@ -92,11 +92,11 @@ void MainWindow::on_pushButton_2_clicked()
 
 void MainWindow::on_pushButton_3_clicked()
 {
-    QFile file("output.xml");
+    QFile file("output.xml"); //Создание файла в корне с программой
     file.open(QIODevice::WriteOnly);
-    QTextStream writeStream(&file);
+    QTextStream writeStream(&file); //Открытие в режиме записи
     writeStream << "<root>\n";
-    for(int i=0;i<jsonList.size();i++){
+    for(int i=0;i<jsonList.size();i++){//Запись всего массива в файл построчно
         writeStream << "<item Binding='Introduced'>\n";
         writeStream << "    <node-path>"+jsonList[i]->key+"</node-path>\n";
         writeStream << "    <address>"+jsonList[i]->value+"</address>\n";
